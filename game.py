3c90b2d59098115960da3f3a9a8422d6e21f4566
Jonathan Marshall <jonkylemars@yahoo.com>
2022-05-16 14:38:50 -0500

    Framework setup

---
+++ game.py
@@ -0,0 +1,21 @@
+def display_welcome():
+    pass
+# Welcome to RPSLS Rock, Paper, Scissors, Lizard, Spock!
+
+def display_rules():
+    pass
+# Rock crushes Scissors 
+# Scissors cuts Paper 
+# Paper covers Rock 
+# Rock crushes Lizard 
+# Lizard poisons Spock 
+# Spock smashes Scissors 
+# Scissors decapitates Lizard 
+# Lizard eats Paper
+# Paper disproves Spock 
+# Spock vaporizes Rock
+
+
+def number_of_players():
+    pass 
+# User input on number of players
